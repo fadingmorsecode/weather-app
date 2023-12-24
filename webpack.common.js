18733239cb78a7d2fc36ae76e94e82d093858cf4
Drake Williamson <williamson.drake@outlook.com>
2023-12-23 23:38:00 -0800

    Intiial commit

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
