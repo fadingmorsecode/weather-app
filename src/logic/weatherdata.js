72b77f44c5270bf4e228d2279604a0bc88afdf17
Drake Williamson <williamson.drake@outlook.com>
2023-12-28 12:41:55 -0800

    Use HTTPS instead of HTTP for api calls

--- src/logic/weatherdata.js
+++ src/logic/weatherdata.js
@@ -2,7 +2,7 @@
 
 export async function getCurrentWeatherData(search) {
   const currentResponse = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=e27d0611824649cba3880002231612&q=${search}&aqi=no`,
+    `https://api.weatherapi.com/v1/current.json?key=e27d0611824649cba3880002231612&q=${search}&aqi=no`,
     { mode: "cors" },
   );
   const currentWeatherData = await currentResponse.json();
@@ -11,7 +11,7 @@ export async function getCurrentWeatherData(search) {
 
 export async function getForecastWeatherData(search) {
   const forecastResponse = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=e27d0611824649cba3880002231612&q=${search}&days=3&aqi=no&alerts=no`,
+    `https://api.weatherapi.com/v1/forecast.json?key=e27d0611824649cba3880002231612&q=${search}&days=3&aqi=no&alerts=no`,
     { mode: "cors" },
   );
   const forecastWeatherData = await forecastResponse.json();
