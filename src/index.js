7ad65db4880395dba67bba8b1f60b5c97713b9b0
Drake Williamson <williamson.drake@outlook.com>
2023-12-28 12:34:16 -0800

    Jesterfy the styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import css from "./main.css";
 import loadDelegation from "./logic/controller";
-import createTemplate from "./dom/template";
-import deleteTemplate from "./dom/templatedeletion";
 
 loadDelegation();
